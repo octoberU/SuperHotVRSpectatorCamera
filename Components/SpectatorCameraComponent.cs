d54e8044b61aa9c25cbdb79f13e820530c5a04bf
octo <allblvnk@gmail.com>
2020-12-30 18:47:12 +0000

    Added local files.

---
+++ Components/SpectatorCameraComponent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using MelonLoader;
+using System;
+
+namespace SpectatorCamera
+{
+    public class SpectatorCameraComponent : MonoBehaviour
+    {
+        Transform playerHead;
+        Camera spectatorCamera;
+
+        void Start()
+        {
+            spectatorCamera = GetComponent<Camera>();
+            
+            spectatorCamera.stereoTargetEye = StereoTargetEyeMask.None;
+            spectatorCamera.fieldOfView = 120f;
+            spectatorCamera.targetDisplay = 0;
+            spectatorCamera.depth = 100;
+            spectatorCamera.nearClipPlane = 0.01f;
+
+            foreach (Camera camera in FindObjectsOfType<Camera>())
+            {
+                if (camera.name == "Hmd") playerHead = camera.transform;
+            }
+            
+            if (playerHead == null) MelonLogger.Log("Player head not found!", ConsoleColor.Red);
+            if (spectatorCamera == null) MelonLogger.Log("Camera not found!", ConsoleColor.Red);
+        }
+
+        void LateUpdate()
+        {
+            if (playerHead == null)
+            {
+                playerHead = VrPlayerAvatar.instance.Head;
+            }
+            else if (spectatorCamera == null) MelonLogger.Log("Camera not found!", ConsoleColor.Red);
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, playerHead.position, 0.9f * Time.unscaledDeltaTime);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, playerHead.rotation, 50f * Time.unscaledDeltaTime);
+            }
+        }
+    }
+}
---
+++ SpectatorCameraMod.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using MelonLoader;
+using SpectatorCamera;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: AssemblyVersion(SpectatorCameraMod.VERSION)]
+[assembly: AssemblyFileVersion(SpectatorCameraMod.VERSION)]
+[assembly: MelonGame("SUPERHOT_Team", "SUPERHOT_VR")]
+[assembly: MelonInfo(typeof(SpectatorCameraMod), "Spectator Camera", SpectatorCameraMod.VERSION, "octo")]
+
+public class SpectatorCameraMod : MelonMod
+{
+    public const string VERSION = "0.1.1";
+
+    static bool objectCreated = false;
+
+    [HarmonyPatch(typeof(VrPlayerAvatar), "Awake", new Type[0])]
+    private static class VrPlayerAvatar_Awake
+    {
+        private static void Postfix(VrPlayerAvatar __instance)
+        {
+            if (objectCreated) return;
+            MelonLogger.Log("Starting SpectatorCam");
+
+            var spectatorCamObject = new GameObject("SpectatorCamera", new Type[]
+            {
+                typeof(SpectatorCameraComponent),
+                typeof(Camera)
+            }
+            );
+            spectatorCamObject.GetComponent<SpectatorCameraComponent>().enabled = true;
+            
+            spectatorCamObject.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            GameObject.DontDestroyOnLoad(spectatorCamObject);
+            objectCreated = true;
+        }
+    }
+}
